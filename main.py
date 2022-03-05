576669c954259ab9aa8922a6099c9d4038911f18
alexey <nake8998@gmail.com>
2022-03-05 20:18:40 +0200

    authorisation with device settings dump in database

--- main.py
+++ main.py
@@ -4,21 +4,17 @@ import sqlite3
 from instagrapi import Client
 
 
-class Data:
-
-    def __init__(self, file_name: str = None):
-        self.con = sqlite3.connect(file_name)
-        self.cur = self.con.cursor()
-
-
 class ClientInterface:
 
     def __init__(self):
-        user_id, device, username, password = Data('data.db').con.execute('select * from user_data').fetchone()
+        self.con = sqlite3.connect('data.db')
+        self.cur = self.con.cursor()
+        self.user_id, self.device, self.username, self.password = \
+            self.cur.execute('select * from user_data').fetchone()
         self.client = Client()
-        self.client.set_device()
 
     def login(self):
+        self.client.set_device()
         self.client.login(self.username, self.password)
 
     def set_device(self):
@@ -27,8 +23,9 @@ class ClientInterface:
             self.client.set_device(device)
 
     def dump_device(self):
-        device_dump = json.dumps(self.client.get_settings()).encode('utf-8')
-        self.con.execute('update user_data set device = ? where id = ?', (device_dump, self.user_id))
+        device_dump = json.dumps(self.client.get_settings())
+        self.cur.execute('update user_data set device = ? where id = ?', (device_dump, self.user_id))
+        self.con.commit()
 
     def __enter__(self):
         self.set_device()
@@ -36,3 +33,8 @@ class ClientInterface:
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.dump_device()
+
+
+if __name__ == '__main__':
+    with ClientInterface() as client:
+        client.login()
--- test.py
+++
@@ -1,7 +0,0 @@
-def get_agemt():
-    with open('user-agents_instagram-app.txt') as file:
-        agets = file.read().splitlines()[::-1]
-        for i in agets:
-            yield i
-
-print(next(get_agemt()))
